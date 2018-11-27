af6267fb32b4b6399fa9b3e42719b8edd808d39a
inkanimal <theatomproject@gmail.com>
2018-11-26 23:50:36 -0500

    created product controller

--- app/controllers/migrate/20180927192401_create_team_players.rb
+++
@@ -1,8 +0,0 @@
-class CreateTeamPlayers < ActiveRecord::Migration
-  def change
-    create_table :team_players do |t|
-    t.integer :team_id
-    t.integer :player_id
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_022232) do
+ActiveRecord::Schema.define(version: 2018_11_26_183539) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.decimal "price", precision: 8, scale: 2, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
