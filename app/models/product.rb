c206a605021fb34f7d2f33afcab1f36da21598b8
inkanimal <theatomproject@gmail.com>
2018-11-28 12:31:09 -0500

    update to git ignore

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ApplicationRecord
 
+  mount_uploader :image, ImageUploader
+  serialize :image, JSON # If you use SQLite, add this line
+
+  has_many :product_categories, dependent: :destroy
+  has_many :categories, through: :product_categories
+
+  has_many :orders
+  has_many :users, through: :orders
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_015007) do
+ActiveRecord::Schema.define(version: 2018_11_28_162516) do
 
   create_table "products", force: :cascade do |t|
     t.string "name", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_11_28_015007) do
     t.decimal "price", precision: 8, scale: 2, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
